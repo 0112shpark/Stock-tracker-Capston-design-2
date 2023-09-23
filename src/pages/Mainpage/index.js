951f1da4f59ed0aeda57b9ce41bfe85e38994c22
0112shpark <000112shpark@naver.com>
2023-09-23 22:54:04 +0900

    :bug:small fix

--- src/pages/Mainpage/index.js
+++ src/pages/Mainpage/index.js
@@ -1,11 +1,20 @@
 import React from "react";
 import Nav from "../../components/Nav";
-
+import { Pulsar } from "@uiball/loaders";
+import { Dna } from "react-loader-spinner";
 const MainPage = () => {
   return (
     <>
       <Nav />
-      <div>MainPage</div>
+      <div>MainPage</div>\
+      {/* <Dna
+        visible={true}
+        height="80"
+        width="80"
+        ariaLabel="dna-loading"
+        wrapperStyle={{}}
+        wrapperClass="dna-wrapper"
+      /> */}
     </>
   );
 };
